b1e0d0e9b5f129d51e8f593619f17c7f3d1f6971
Argead <michaelmackinnon@outlook.com>
2018-11-23 18:29:52 +0000

    moved solidity contracts to correct dir, and created a prototype to test event logging

---
+++ ethereum/contracts/Pausable.sol
@@ -0,0 +1,32 @@
+pragma solitity ^0.4.25;
+
+import "./Ownable.sol";
+
+contract Pausable is Ownable {
+  event Pause();
+  event Unpause();
+
+  bool public paused = false;
+
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+
+  modifier whenPaused {
+    require(paused);
+    _;
+  }
+
+  function pause() onlyOwner whenNotPaused returns (bool) {
+    paused = true;
+    Pause();
+    return true;
+  }
+
+  function unpause() onlyOwner whenPaused returns (bool) {
+    paused = false;
+    Unpause();
+    return true;
+  }
+}
---
+++ ethereum/contracts/Prototype.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.25;
+
+contract Prototype {
+    event NewContent(address owner, uint256 contentId, string name);
+
+    Content[] textContent;
+
+    struct Content {
+      string name;
+    }
+
+    function createContent(string _name) public returns (uint) {
+      Content memory _content = Content({
+        name: _name
+      });
+      uint256 newContentId = textContent.push(_content) - 1;
+      require(newContentId == uint256(uint32(newContentId)));
+      emit NewContent(msg.sender, newContentId, _name);
+      return newContentId;
+    }
+}
